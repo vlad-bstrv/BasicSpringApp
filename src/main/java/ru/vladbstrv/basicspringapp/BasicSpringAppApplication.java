8265486999bb1afd4f0f6b5f96c4c02a605ed2b2
vlad-bstrv <keocera@icloud.com>
2024-06-07 10:03:41 +0300

    Initial commit

---
+++ src/main/java/ru/vladbstrv/basicspringapp/BasicSpringAppApplication.java
@@ -0,0 +1,13 @@
+package ru.vladbstrv.basicspringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicSpringAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/vladbstrv/basicspringapp/BasicSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vladbstrv.basicspringapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicSpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
