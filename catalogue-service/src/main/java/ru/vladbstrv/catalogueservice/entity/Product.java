ea8c2a5af849635b04fe37f415459fa40c544dd5
vlad-bstrv <keocera@icloud.com>
2024-06-08 16:31:50 +0300

    db schema

--- catalogue-service/src/main/java/ru/vladbstrv/catalogueservice/entity/Product.java
+++ catalogue-service/src/main/java/ru/vladbstrv/catalogueservice/entity/Product.java
@@ -1,5 +1,8 @@
 package ru.vladbstrv.catalogueservice.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,9 +10,20 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(schema = "catalogue", name = "t_product")
 public class Product {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @Column(name = "c_title")
+    @NotNull
+    @Size(min = 3, max = 50)
     private String title;
+
+    @Column(name = "c_details")
+    @Size(max = 1000)
     private String details;
 }
