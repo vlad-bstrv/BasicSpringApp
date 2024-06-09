939a8d842e294d226509ecfc25362d8c50a364b2
vlad-bstrv <keocera@icloud.com>
2024-06-09 13:16:26 +0300

    basic authentication on Rest api

--- catalogue-service/src/main/java/ru/vladbstrv/catalogueservice/repository/ProductRepository.java
+++ catalogue-service/src/main/java/ru/vladbstrv/catalogueservice/repository/ProductRepository.java
@@ -4,8 +4,6 @@ package ru.vladbstrv.catalogueservice.repository;
 import org.springframework.data.repository.CrudRepository;
 import ru.vladbstrv.catalogueservice.entity.Product;
 
-import java.util.List;
-import java.util.Optional;
 
 public interface ProductRepository extends CrudRepository<Product, Integer> {
 
