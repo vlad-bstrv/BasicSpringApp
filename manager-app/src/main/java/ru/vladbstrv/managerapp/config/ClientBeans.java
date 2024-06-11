e2087c33563b8f924e4f19814070c761bcbd3068
vlad-bstrv <keocera@icloud.com>
2024-06-11 13:03:15 +0300

    schema manager

--- manager-app/src/main/java/ru/vladbstrv/managerapp/config/ClientBeans.java
+++ manager-app/src/main/java/ru/vladbstrv/managerapp/config/ClientBeans.java
@@ -3,6 +3,7 @@ package ru.vladbstrv.managerapp.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.support.BasicAuthenticationInterceptor;
 import org.springframework.web.client.RestClient;
 import ru.vladbstrv.managerapp.client.RestClientProductsRestClient;
 
@@ -11,11 +12,13 @@ public class ClientBeans {
 
     @Bean
     public RestClientProductsRestClient productsRestClient(
-            @Value("${selmag.services.catalogue.uri:http://localhost:8081}") String catalogueBaseUri
-    ) {
+            @Value("${selmag.services.catalogue.uri:http://localhost:8081}") String catalogueBaseUri,
+            @Value("${selmag.services.catalogue.username:}") String catalogueUsername,
+            @Value("${selmag.services.catalogue.password:}") String cataloguePassword) {
         return new RestClientProductsRestClient(
                 RestClient.builder()
                         .baseUrl(catalogueBaseUri)
+                        .requestInterceptor(new BasicAuthenticationInterceptor(catalogueUsername, cataloguePassword))
                         .build()
         );
     }
