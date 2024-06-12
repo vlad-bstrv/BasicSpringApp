2f8a52eea502696a7292ecb332e317264854464e
vlad-bstrv <keocera@icloud.com>
2024-06-12 15:01:08 +0300

    keycloak

---
+++ manager-app/src/main/java/ru/vladbstrv/managerapp/config/SecurityBeans.java
@@ -0,0 +1,19 @@
+package ru.vladbstrv.managerapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityBeans {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests.anyRequest().authenticated())
+                .oauth2Login(Customizer.withDefaults())
+                .build();
+    }
+}
